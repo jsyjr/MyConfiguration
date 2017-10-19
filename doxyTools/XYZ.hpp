1a2c43ba1437f9453a09f1667f638cf735573dca
John S. Yates, Jr <john@yates-sheets.org>
2017-10-19 17:18:37 -0400

    New subtree: $(HOME)/doxyTools/*

---
+++ doxyTools/XYZ.hpp
@@ -0,0 +1,10 @@
+//========================================================================
+// Copyright 2017 The MathWorks, Inc.
+//
+///\doco{ XYZ }
+//========================================================================
+
+namespace XYZ
+  /// The XYZ namespace comprises the XYZ abstraction.
+{
+}
