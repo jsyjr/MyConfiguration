020666a8c5be0d44ef685837fdf68c73e262a804
John S. Yates, Jr <john@yates-sheets.org>
2017-12-06 09:13:25 -0500

    Kill ~/.srinatha/

--- .srinath/sbtools/findInProj.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-
-from ListSearch import listOrSearchFiles, Finder
-
-if __name__ == "__main__":
-    print "\n".join(listOrSearchFiles(1, Finder))
--- .srinath/sbtools/findInSoln.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-
-import sys
-from ListSearch import listOrSearchFiles, Finder
-
-if __name__ == "__main__":
-    print "\n".join(listOrSearchFiles(0, Finder))
