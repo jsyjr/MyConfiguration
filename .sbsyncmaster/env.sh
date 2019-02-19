5cf5c3a18c9fbe2155399be39e7d89cce0274864
John S. Yates, Jr <john@yates-sheets.org>
2019-02-19 12:20:52 -0500

    add ~/syncmaster/env.sh

---
+++ .sbsyncmaster/env.sh
@@ -0,0 +1,28 @@
+#!/bin/dash
+# CAVEAT! cron picks up my network HOME (= /home/jyates) rather than my local version (= /jyates).
+# This line ensures that the environment supplied to invoked commands specifies the correct HOME.
+export HOME=/jyates
+
+# Should match ${HOME}/.profile
+export CCACHE_DIR=/ccc
+export CCACHE_LOGFILE=/ccc/LOGFILE
+export CCACHE_SLOPPINESS=include_file_mtime,file_macro,time_macros
+export CCACHE_TEMPDIR=/huge/ccache
+export USE_CCACHE=1
+
+#############################################################
+#
+# The sync-and-build script expects these in the environment:
+
+### sbsyncmaster scripts and logs (echoed verbatim below because cron does no symbol substitution)
+export SYNCMASTERDIR=/jyates/.sbsyncmaster
+
+### Where sbsyncmaster should create latest_pass areas
+export LATESTPASSDIR=/ws
+
+### Information for git.
+export GIT_USER_NAME=jyates
+export GIT_USER_EMAIL=jyates@mathworks.com
+
+# Now run the actual script
+${SYNCMASTERDIR}/sync-and-build
