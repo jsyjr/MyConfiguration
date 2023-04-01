7a2167c1551cfe32bb38e7dfe8e156d0c977eed1
John S. Yates, Jr <john@yates-sheets.org>
2023-03-31 21:07:13 -0400

    init.el: cleanup iosevka-comfy formatting

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -840,9 +840,9 @@ as in `defun'."
 
   (let
 ;;    ((fixed-family "Monoid") (variable-family "Source Sans Pro"))
-; ;https://git.sr.ht/~protesilaos/iosevka-comfy
-    ((fixed-family "Iosevka Comfy Fixed") (variable-family "Iosevka Comfy Duo"))
-;;  ((fixed-family "Source Code Pro") (variable-family "Source Sans Pro"))
+;; https://git.sr.ht/~protesilaos/iosevka-comfy
+   ((fixed-family "Iosevka Comfy Fixed") (variable-family "Iosevka Comfy Duo"))
+;;    ((fixed-family "Source Code Pro") (variable-family "Source Sans Pro"))
 ;;    ((fixed-family "Terminus") (variable-family "Source Sans Pro"))
 ;;    ((fixed-family "Input Mono") (variable-family "Input Sans"))
 ;;    ((fixed-family "Dina") (variable-family "Input Sans"))
